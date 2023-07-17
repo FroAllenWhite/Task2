759eef643c8e8bb9fccdda6b72dd7f28a0ebe283
FroAllenWhite <118559993+FroAllenWhite@users.noreply.github.com>
2023-07-17 13:34:18 +0400

    create migrations

--- config/db.php
+++ config/db.php
@@ -6,7 +6,6 @@ return [
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-    'tablePrefix' => 'tbl_',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
