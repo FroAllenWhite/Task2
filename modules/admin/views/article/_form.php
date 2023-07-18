60848c3bab8be4d01e2b12fa1d9c5abfa7ec8047
FroAllenWhite <118559993+FroAllenWhite@users.noreply.github.com>
2023-07-18 14:32:19 +0400

    picture uploading

--- modules/admin/views/article/_form.php
+++ modules/admin/views/article/_form.php
@@ -20,18 +20,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'date')->textInput() ?>
 
-    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'viewed')->textInput() ?>
-
-    <?= $form->field($model, 'iser_id')->textInput() ?>
-
-    <?= $form->field($model, 'status')->textInput() ?>
-
-    <?= $form->field($model, 'category_id')->textInput() ?>
-
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Create', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
